c3aa5f9e3e81fa01b0df88cc093e504986e65dba
MatyTan <matytan@bituniverse.org>
2023-06-07 11:22:07 +0800

    add effective encode/decode

--- zippool_test.go
+++ zippool_test.go
@@ -7,7 +7,12 @@ import (
 	"testing"
 )
 
-var zipPool = NewZipPool(gzip.BestCompression)
+var zipPool = NewZipPoolLevel(gzip.BestCompression)
+
+type Data struct {
+	Str string
+	I   int64
+}
 
 func TestZipPool(t *testing.T) {
 
@@ -27,4 +32,28 @@ func TestZipPool(t *testing.T) {
 			t.Fatalf("Decompressed data does not match original data: got %s, want %s", decompressedData, data)
 		}
 	}
+
+	// dataD := &Data{
+	// 	Str: "Hello, Golang zip pool!",
+	// 	I:   1234567890,
+	// }
+	//
+	// for i := 0; i < 10; i++ {
+	// 	// struct to []byte
+	//
+	// 	compressedData1, err := zipPool.Compress(dataB)
+	// 	if err != nil {
+	// 		t.Fatalf("Failed to compress data: %v", err)
+	// 	}
+	//
+	// 	decompressedData1, err := zipPool.Decompress(compressedData1)
+	// 	if err != nil {
+	// 		t.Fatalf("Failed to decompress data: %v", err)
+	// 	}
+	//
+	// 	if !bytes.Equal(dataB, decompressedData1) {
+	// 		t.Fatalf("Decompressed data does not match original data: got %s, want %s", decompressedData1, compressedData1)
+	// 	}
+	// }
+
 }
